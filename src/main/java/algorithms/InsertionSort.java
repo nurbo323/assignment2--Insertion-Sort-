2c7315f0f9d6faed50d006952cd475e5023d70b7
nurbol amangeldinov <nurbolamangeldinov10@gmail.com>
2025-10-05 20:16:31 +0500

    feat(algorithm): implemented baseline Insertion Sort

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,4 +1,19 @@
 package algorithms;
 
-public class InsertionSort git add src/main/java/algorithms/InsertionSort.java{
+public class InsertionSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null) return; // простая валидация
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            // Сдвигаем элементы, большие key, вправо
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
