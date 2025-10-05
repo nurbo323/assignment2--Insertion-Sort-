12be1685e0214a9c12c1e234e3f00e6f39a998ef
nurbol amangeldinov <nurbolamangeldinov10@gmail.com>
2025-10-05 20:40:25 +0500

    feat(algorithm): integrated PerformanceTracker into InsertionSort

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,19 +1,26 @@
 package algorithms;
 
+import metrics.PerformanceTracker;
+
 public class InsertionSort {
 
-    public static void sort(int[] arr) {
-        if (arr == null) return; // простая валидация
+    public static void sort(int[] arr, PerformanceTracker tracker) {
+        if (arr == null) return;
         int n = arr.length;
+        tracker.start();
         for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
-            // Сдвигаем элементы, большие key, вправо
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+            while (j >= 0) {
+                tracker.incrementComparisons();
+                if (arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    tracker.incrementSwaps();
+                    j--;
+                } else break;
             }
             arr[j + 1] = key;
         }
+        tracker.stop();
     }
 }
